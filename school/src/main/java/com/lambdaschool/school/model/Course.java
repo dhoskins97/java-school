7ea8a99f62d500eaa93d196bcff467a21d1b85d1
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-09-16 18:37:48 -0400

    change made for initial commit

--- school/src/main/java/com/lambdaschool/school/model/Course.java
+++ school/src/main/java/com/lambdaschool/school/model/Course.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+// initial change
 @Entity
 @Table(name = "course")
 public class Course
